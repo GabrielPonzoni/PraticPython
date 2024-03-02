5f08e11fe222aab30fea8a98b392666887d25e66
Gabriel "Syll3x" Ponzoni <gabrielponzoni1234@gmail.com>
2024-03-02 17:54:05 -0300

    2.0
    Mais umas questoes

---
+++ EstruturaSequencial/02_SomaDeDoisNumeros.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input('Informe um primeiro numero para a soma: '))
+numero2 = int(input('Informe o segundo numero para ser somado: '))
+
+resultado = numero1 + numero2
+
+print(f'{numero1} + {numero2} = {resultado}')

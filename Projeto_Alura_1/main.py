a2cb3c5537a1ed2d0f72669be0c06ff53b01f9e7
Gabriel "Syll3x" Ponzoni <gabrielponzoni1234@gmail.com>
2024-05-07 11:32:09 -0300

    Alura 4.0
    Dicionario

--- Projeto_Alura_1/main.py
+++ Projeto_Alura_1/main.py
@@ -18,7 +18,7 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
+    print('3. Alternar status do restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
@@ -34,7 +34,11 @@ def opcao_invalida():
 
 def exibir_subtitulo(texto):
     os.system('cls')
+    linha = '*' * (len(texto) + 4)
+    
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def cadastrar_novo_restaurante():
@@ -51,14 +55,37 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
 
+    cabecalho = f'  {"Nome:".ljust(20)} | {"Categoria:".ljust(20)} | Status:'
+    
+    print(cabecalho)
+    print(f'-' * (len(cabecalho)))
+    
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria_restaurante = restaurante['categoria']
-        status_restaurante = restaurante['ativo']
-        print(f'- {nome_restaurante} | {categoria_restaurante} | {status_restaurante}')
+        status_restaurante = 'Ativado' if restaurante['ativo'] else 'Desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria_restaurante.ljust(20)} | {status_restaurante}')
 
     voltar_ao_menu_principal()
+    
+def alternar_valor_restaurante():
+    exibir_subtitulo('Alterando estado do restaurante')
 
+    restaurante_encontrado = False
+    nome_restaurante = input('Informe o nome do restaurante que deseja ativar ou desativar: ')
+    
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']         # troca o valor True ou False do restaurante 
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso!' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso!'
+            print(mensagem)
+    
+    if not restaurante_encontrado:
+        print(f'O restaurante {nome_restaurante} não foi encontrado!')
+    
+    voltar_ao_menu_principal()
+    
 def escolher_opcao():
     
     try:
@@ -70,11 +97,11 @@ def escolher_opcao():
             case 2:
                 listar_restaurantes()
             case 3:
-                print('Ativar restaurante')         
+                alternar_valor_restaurante()        
             case 4:
                 finalizar_app()
             case _:
-                print('Não está dentro das opções!')
+                print(f'A resposta {opcao_escolhida} não aestá dentro das opções!')
     except:
         opcao_invalida()
         

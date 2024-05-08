f4b381694ffa895cf0bcee4ddcd53e6f0a219fb5
Gabriel "Syll3x" Ponzoni <gabrielponzoni1234@gmail.com>
2024-05-08 13:57:40 -0300

    Alura 6.0
    Criado a main.py

---
+++ ProjetoPOO_Alura_2/main.py
@@ -0,0 +1,16 @@
+from restaurantes import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_mexicano.alternar_estado()
+
+def main():
+    
+    Restaurante.listar_restaurantes()
+
+
+
+if __name__ == '__main__':
+    main()
